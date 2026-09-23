351adc0c37e64f63912edd9b5a83365bdcb79832
oowla <diankastrbova@gmail.com>
2026-09-23 13:34:57 +0100

    Add files via upload

--- cvicenie1.sql
+++ cvicenie1.sql
@@ -1,3 +1,16 @@
--- Active: 1790162492317@@127.0.0.1@5432@Northwind
-
-
+-- Active: 1790162492317@@127.0.0.1@5432@superstore
+CREATE TABLE orders (   
+    order_id VARCHAR(20) PRIMARY KEY,
+    customer_id VARCHAR(20),
+    FOREIGN KEY (customer_id) 
+        REFERENCES customers(customer_id),
+    product_id VARCHAR(20),
+    FOREIGN KEY (product_id) 
+        REFERENCES products(product_id),
+    order_date DATE,
+    ship_date DATE,
+    sales NUMERIC(18, 2),
+    quantity NUMERIC(18,0),
+    discount NUMERIC(18,2),
+    profit NUMERIC(18,2)
+);
