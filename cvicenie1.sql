18437e374441f4a7070b8dbd61d98cf652476564
oowla <diankastrbova@gmail.com>
2026-09-23 19:12:03 +0200

    Update cvicenie1.sql

--- cvicenie1.sql
+++ cvicenie1.sql
@@ -1,4 +1,18 @@
--- Active: 1790162492317@@127.0.0.1@5432@superstore
+CREATE TABLE customers (   
+    customer_id VARCHAR(20) PRIMARY KEY,
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+    country VARCHAR(50),
+    region VARCHAR(50)
+);
+
+CREATE TABLE products (
+    product_id VARCHAR(20) PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    sub_category VARCHAR(50)
+)
+
 CREATE TABLE orders (   
     order_id VARCHAR(20) PRIMARY KEY,
     customer_id VARCHAR(20),
